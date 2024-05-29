def66ab0051e11a728fe0d4814c3744786da16f4
kamalj57 <kamal452jk@gmail.com>
2024-05-29 21:10:38 +0530

    updated

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -102,7 +102,6 @@ export const Img = styled.img`
   max-height: 400px;
   border-radius: 50%;
   border: 2px solid ${({ theme }) => theme.primary};
-
   @media (max-width: 768px) {
     max-width: 400px;
     max-height: 400px;
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,4 @@
 import Figma from './assets/Figma-color.svg';
-import vscode from './assets/vs-code.png';
 import mysql from './assets/mysql.png';
 import mongodb from './assets/mongodb.png';
 import html from './assets/html.png';
@@ -17,7 +16,7 @@ import emdc from './assets/emdc.jpeg'
 import clsrep from './assets/clsrep.png'
 import eventmanagement from './assets/eventmanagement.png'
 import hospitalmanagement from './assets/hospitalmanagement.png'
-import eldery from './assets/eldery.png'
+import elderly from './assets/eldery.png'
 import whatsappclone from './assets/whatsappclone.png'
 import  instaclone from './assets/instaclone.jpg'
 import  plantdisease from './assets/plantdisease.png'
@@ -46,22 +45,22 @@ export const skills = [
     title: "Frontend",
     skills: [
       {
+        name: "JavaScript",
+        image: js ,
+
+      },
+      {
+        name: "React",
+        image: reatcjs ,
+      },
+      {
         name: "HTML",
         image:html,
       },
       {
         name: "CSS",
         image:  css ,
-      },
-      {
-        name: "JavaScript",
-        image: js ,
-
-      },
-      {
-        name: "React Js",
-        image: reatcjs ,
-      },
+      }
 
     ],
   },
@@ -69,11 +68,11 @@ export const skills = [
     title: "Backend",
     skills: [
       {
-        name: "Node Js",
+        name: "Node.js",
         image: nodejs ,
       },
       {
-        name: "Express Js",
+        name: "Express.js",
         image: Expressjs,
       },
       {
@@ -94,10 +93,6 @@ export const skills = [
         image:  github ,
       },
       {
-        name: "VS Code",
-        image:  vscode ,
-      },
-      {
         name: "Figma",
         image:Figma,
          },
@@ -116,23 +111,23 @@ export const education = [
     id: 0,
     img: kec,
     school: "Kongu Engineering College, Erode",
-    date: "NOV 2021 - NOV 2025",
+    date: "Nov 2021 - Nov 2025",
     grade: "8.55 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kongu Engineering College, Erode. I have completed 5 semesters and have a CGPA of 8.55. I have taken courses in Data Structures Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kongu Engineering College, Erode. I have completed 5 semesters and have a CGPA of 8.55. Additionally, I am studying for an Honours degree with specialization in Data Science",
     degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
   },
   {
     id: 1,
     school: "Venganar Vajram Matric Hr Sec School",
-    date: "MAY 2021",
+    date: "May 2021",
     grade: "93.06%",
-    desc: "I completed my class 12 high school education at Venganar Vajram Matric Hr Sec School, where I studied Maths Biology.",
+    desc: "I completed my class 12 high school education at Venganar Vajram Matric Hr Sec School.",
     degree: "HSC(XII), Maths Biology",
   },
   {
     id: 2,
     school: "Venganar Vajram Matric Hr Sec School",
-    date: "MAR 2019",
+    date: "Mar 2019",
     grade: "90.4%",
     desc: "I completed my class 10 education at Venganar Vajram Matric Hr Sec School.",
     degree: "SSLC(X)",
@@ -144,15 +139,9 @@ export const projects = [
     id: 0,
     title: "AI IMAGE GENERATOR",
     date: "Dec 2023 - Jan 2024",
-    description:
-      "Developed a full-stack web application that allows users to generate ai images using Openai-Dalle3 and cloudinary for image storage ",
-    image:dalle,
-    tags: [
-      "MERN Stack",
-      "Cloudinary",
-      "OpenAi",
-      "Dalle-3",
-    ],
+    description: "Developed a full-stack web application that allows users to generate AI images using OpenAI DALL-E  and Cloudinary for image storage.",
+    image: dalle,
+    tags: ["MERN", "Cloudinary", "OpenAI DALL-E"],
     category: "web app",
     github: "https://github.com/kamalj57/Dall-E",
     videoDemo: "https://drive.google.com/file/d/1_Xpv0L95YX6v1ZQLt6EIJR7x53qOZv53/view?usp=drive_link",
@@ -161,53 +150,42 @@ export const projects = [
     id: 1,
     title: "Expense Tracker",
     date: "Jan 2024 - Jan 2024",
-    description:
-      "Developed a full-stack web application that allows users to manage thier income and expenses.",
-    image:expensetracker,
-    tags: ["React Js", "MongoDb", "Node Js", "Express Js"],
+    description: "Developed a full-stack web application that allows users to manage their income and expenses.",
+    image: expensetracker,
+    tags: ["MERN", "React-Chartjs"],
     category: "web app",
     github: "https://github.com/kamalj57/Expense_tracker",
     webapp: "https://expense-tracker-jk.vercel.app/",
-
   },
   {
     id: 2,
-    title: "KEC Vechile Management System",
-    date: "Feb 2024 - FEb 2024",
-    description:
-      "Developed a full-stack web application that allows users to know  prior notification of bus services using MERN stack and twilio for sending notifications and node cron for schedule in the event of E-Horyzon'24",
-    image:kecvms,
-    tags: ["React Js", "MongoDb", "Node Js", "Express Js","Twilio"],
+    title: "KEC Vehicle Management System",
+    date: "Feb 2024 - Feb 2024",
+    description: "Developed a full-stack web application that provides prior notifications of bus services using the MERN stack and Twilio for sending notifications. Node Cron is used for scheduling in the event of E-Horyzon'24 @ KEC.",
+    image: kecvms,
+    tags: ["MERN", "Twilio"],
     category: "web app",
     github: "https://github.com/kamalj57/KEC-VMS",
     webapp: "",
-
   },
   {
     id: 3,
     title: "Infotube",
-    date: "Aug 2023 - Aug2023",
-    description:
-      "Developed the Infotube project, a project management app that helps the students to identify the unique projects and showcase their talents accross India and presented at Smart India Internal Hackathon @Kec",
-    image:infotube,
-     tags: [
-      "React Js",
-      "MongoDb",
-      "Node Js",
-      "Express Js",
-    ],
+    date: "Aug 2023 - Aug 2023",
+    description: "Developed the Infotube project, a project management app that helps students identify unique projects and showcase their talents across India. Presented at the Smart India Internal Hackathon @ KEC.",
+    image: infotube,
+    tags: ["MERN"],
     category: "web app",
     github: "",
     webapp: "",
   },
   {
     id: 4,
-    title: "Event Managemet",
-    date: "Feb-13 2023 - Feb-18 2023",
-    description:
-      "Deveolped an event management app using HTML,CSS and Google Sheets for storage of data in the event of E-Horyzon'23,KEC",
-    image:eventmanagement,
-    tags: ["HTML", "CSS", "Google Sheets"],
+    title: "Event Management",
+    date: "Feb  2023 - Feb 2023",
+    description: "Developed an event management app using HTML, CSS, and Google Sheets for data storage during the event of E-Horyzon'23 @ KEC.",
+    image: eventmanagement,
+    tags: ["HTML/CSS", "Google Sheets"],
     category: "web app",
     github: "",
     webapp: "",
@@ -215,96 +193,93 @@ export const projects = [
   {
     id: 5,
     title: "Hospital Management System",
-    date: "Oct-30 2023 - Nov-03 2023",
-    description:"Devloped an Hospital Management web app with an user-frinedly UI and have modules like appointement booking and the project is developed using the Agile Principles",
-    image:hospitalmanagement,
-    tags: ["ReactJs","MongoDB","Express","NodeJs", "Axios"],
+    date: "Oct  2023 - Nov  2023",
+    description: "Developed a hospital management web app with a user-friendly UI, including modules like appointment booking. The project follows Agile principles.",
+    image: hospitalmanagement,
+    tags: ["MERN"],
     category: "web app",
     github: "https://github.com/kamalj57/HospitalManagement_Agile",
     webapp: "",
   },
   {
-    id: 6,
-    title: "Plant Disease Idnetifier",
-    date: "Oct 2023 - Oct 2023",
-    description:
-      "Developed an Plant Disease Identifier app to diagnoses plant ailments through image recognition, revolutionizing agriculture with precision and efficiency. ",
-    image:plantdisease,
-    tags: ["Flutter", "CNN", "Tenserflow"],
-    category: "android app",
-    github: "https://github.com/kamalj57/PlantDiseaseIdentifierApp_Flutter",
-    webapp: "",
+    
+      id: 6,
+      title: "Plant Disease Identifier_UI",
+      date: "Oct 2023 - Oct 2023",
+      description: "Designed a plant disease identifier app using Flutter, focusing on the user interface.",
+      image: plantdisease,
+      tags: ["Flutter"],
+      category: "android app",
+      github: "https://github.com/kamalj57/PlantDiseaseIdentifierApp_Flutter",
+      webapp: "",
+    
   },
   {
     id: 7,
-    title: "Eldery To-Do",
-    date: "Oct-30 2023 - Nov-03 2023",
-    description:
-      "Developed an Eldery ToDo app that helps to manage the daily tasks of the eldery people in the event of IXplorer as part of Celesta'23 organized by GDSC IIT Patna",
-    image:Dashboard,
-    tags: ["ReactJs","MongoDB","Express","NodeJs", "Axios"],
+    title: "Elderly To-Do",
+    date: "Oct 2023 - Nov 2023",
+    description: "Developed an Elderly To-Do app to help manage the daily tasks of elderly people for the IXplorer event as part of Celesta'23 organized by GDSC IIT Patna.",
+    image: Dashboard,
+    tags: ["MERN"],
     category: "web app",
-    github: "https://github.com/kamalj57/ElderyApp_ToDo",
+    github: "https://github.com/kamalj57/ElderlyApp_ToDo",
     webapp: "",
   },
   {
     id: 8,
     title: "Hospital Landing Page",
     date: "Jun 2023",
-    description:
-      "Desinged the Hospital Landing page  concept UI with showcase the specialize of the hospital using Figma",
-    image:hospital,
-    tags: ["Figma","Landing Page","Hospital UIkit"],
+    description: "Designed a hospital landing page concept UI to showcase the specialties of the hospital using Figma.",
+    image: hospital,
+    tags: ["Figma"],
     category: "design",
-    designLink:"https://dribbble.com/shots/22274140-Web-view-for-Hospital-System?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Web%20view%20for%20Hospital%20System&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Web%20view%20for%20Hospital%20System&utm_medium=Social_Share",
+    designLink: "https://dribbble.com/shots/22274140-Web-view-for-Hospital-System?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Web%20view%20for%20Hospital%20System&utm_medium=Social_Share",
   },
   {
     id: 9,
-    title: "Eldery ToDo_UI",
-    date: "Oct-30 2023 - Nov-03 2023",
-    description:
-      "Designed an Eldery ToDo app that helps to manage the daily tasks of the eldery people in the event of IXplorer as part of Celesta'23 organized by GDSC IIT Patna",
-    image:eldery,
-    tags: ["Figma","ToDo App","Eldery ToDo"],
+    title: "Elderly To-Do_UI",
+    date: "Oct 2023 - Nov 2023",
+    description: "Designed an Elderly To-Do app to help manage the daily tasks of elderly people for the IXplorer event as part of Celesta'23 organized by GDSC IIT Patna.",
+    image: elderly,
+    tags: ["Figma", "To-Do App", "Elderly To-Do"],
     category: "design",
-    designLink:"https://dribbble.com/shots/22969610-Eldery-Remainder-App?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Eldery%20Remainder%20App&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Eldery%20Remainder%20App&utm_medium=Social_Share"
+    designLink: "https://dribbble.com/shots/22969610-Elderly-Remainder-App?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Elderly%20Remainder%20App&utm_medium=Social_Share",
   },
   {
     id: 10,
     title: "Instagram Clone",
     date: "Jun 2023",
-    description:
-      "Desinged the Instagram concept UI Kit using Figma",
-    image:instaclone,
-    tags: ["Figma","Instagram Clone"],
+    description: "Designed an Instagram concept UI Kit using Figma.",
+    image: instaclone,
+    tags: ["Figma"],
     category: "design",
-    designLink:"https://dribbble.com/shots/22932500-Instagram-UI-Kit-2023-1-0?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Instagram%20UI%20Kit%202023%201.0&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Instagram%20UI%20Kit%202023%201.0&utm_medium=Social_Share"
+    designLink: "https://dribbble.com/shots/22932500-Instagram-UI-Kit-2023-1-0?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=Instagram%20UI%20Kit%202023%201.0&utm_medium=Social_Share",
   },
   {
     id: 11,
-    title: "Whatsapp Clone",
+    title: "WhatsApp Clone",
     date: "Jul 2023",
-    description:"Designed an Whatsapp UI Kit with concept of separate groups and chat using Figma ",
-      image:whatsappclone,
-    tags: ["Figma","Whatsapp clone"],
+    description: "Designed a WhatsApp UI Kit with a concept of separate groups and chat using Figma.",
+    image: whatsappclone,
+    tags: ["Figma"],
     category: "design",
-    designLink:"https://dribbble.com/shots/22984529-CHATAPP-1-0?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=CHATAPP%201.0&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=CHATAPP%201.0&utm_medium=Social_Sharehttps://dribbble.com/shots/22984529-CHATAPP-1-0?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=CHATAPP%201.0&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=CHATAPP%201.0&utm_medium=Social_Share"
+    designLink: "https://dribbble.com/shots/22984529-CHATAPP-1-0?utm_source=Clipboard_Shot&utm_campaign=kamalj_57&utm_content=CHATAPP%201.0&utm_medium=Social_Share",
   },
-  
 ];
+
 
 export const volunteering = [
   {
     id: 0,
     img: emdc,
-    position:"EXECUTIVE MEMBER",
-    desc: "Entrepreneurship Management and Development Centre(IIC@EMDC)",
-    date: "SEP 2023",
+    position: "EXECUTIVE MEMBER",
+    desc: "Entrepreneurship Management and Development Centre",
+    date: "Sep 2023",
   },
   {
     id: 1,
     img: clsrep,
-    position:"CLASS REPRESENTATIVE",
-    date: "JUN 2022-DEC 2022",
-  },
+    position: "CLASS REPRESENTATIVE",
+    date: "Jun 2022 - Dec 2022",
+  }  
 ];
